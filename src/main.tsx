ca48e4650f4e1bfb7d9b01563c070df9b4f5ad55
Xiaohw1999 <wuxiaohui990723@gmail.com>
2026-06-02 18:53:08 +0800

    1

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,7 @@ import App from './App'
 import './index.css'
 import '@/i18n'
 
-console.log('[main] React:', typeof React, 'ReactDOM:', typeof ReactDOM, 'createRoot:', typeof ReactDOM?.createRoot)
-const rootEl = document.getElementById('root')
-console.log('[main] root element:', rootEl)
-ReactDOM.createRoot(rootEl!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
